cf9e331914f3d351863d0418fdfcbc1ae80a6385
Martin Heinrich <martin.heinrich.dresden@gmx.de>
2018-06-22 18:41:48 +0200

    added styles.css, make it colored, refactored

--- src/main/kotlin/org/daiv/BetServer.kt
+++ src/main/kotlin/org/daiv/BetServer.kt
@@ -11,6 +11,7 @@
 package org.daiv
 
 import io.ktor.application.call
+import io.ktor.content.*
 import io.ktor.html.respondHtml
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -20,11 +21,102 @@ import kotlinx.coroutines.experimental.launch
 import kotlinx.html.*
 import org.daiv.bet.getResults
 import org.daiv.immutable.utils.persistence.annotations.DatabaseWrapper
+import java.io.File
 import java.text.SimpleDateFormat
 
 fun toLong(string: String): Long {
     val parse = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(string)
     return parse.time
+}
+
+fun toOtherFormat(string: String): String {
+    return SimpleDateFormat("dd.MM.yyyy HH:mm").format(SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(string))
+}
+
+@HtmlTagMarker
+inline fun TR.head(users: List<User>) {
+    td("head") { +"Anstoßzeit" }
+    td("head") { +"Begegnung" }
+    td("head") { +"Ergebnis" }
+    users.forEach {
+        td("head") { +it.name }
+        td("head") {}
+    }
+}
+
+@HtmlTagMarker
+inline fun TR.subHead(users: List<User>) {
+    td("column1") { }
+    td("column1") { }
+    td("result") { }
+    users.forEach {
+        td { +"Tipp" }
+        td { +"Punkte" }
+    }
+}
+
+@HtmlTagMarker
+inline fun TR.bets(result: BetData, users: List<User>, bets: List<PointData>) {
+    val match = result.betKey.match
+    td("column1") { +toOtherFormat(match.date) }
+    td("column1") { +match.toString() }
+    td("result") { +result.result.toString() }
+    users.forEach { user ->
+        bets.find { bet ->
+            bet.betData.betKey.user == user && match.equalsSameDayAndTeam(bet.betData.betKey.match)
+        }?.let {
+            td { +"${it.betData.result} " }
+            if (it.betData.betKey.user != User.RESULT) {
+                td { +"${it.points} " }
+            }
+        } ?: run {
+            td { }
+            td { }
+        }
+    }
+}
+
+@HtmlTagMarker
+inline fun TR.footer(users: List<User>, bets: List<PointData>) {
+    td("head") {}
+    td("head") {}
+    td("head") { +"Zwischenstand: " }
+    users.forEach { user ->
+        val sum = bets.filter { it.betData.betKey.user == user }
+            .map { it.points }
+            .filter { it != Points.NONE }
+            .map(Points::int)
+            .sum()
+        td("head") { +"${user.name}" }
+        td("head") { +"$sum" }
+    }
+}
+
+@HtmlTagMarker
+inline fun BODY.thisBody(d: BetDatabase, head: String) {
+    val users = d.getUsers()
+        .filter { it != User.RESULT }
+    val (res, bets) = getBets(d.readAll())
+    val results = res.sortedWith(compareBy { toLong(it.betKey.match.date) })
+    h1 { +head }
+    p {
+        table {
+            tr {
+                head(users)
+            }
+            tr {
+                subHead(users)
+            }
+            results.forEach { result ->
+                tr {
+                    bets(result, users, bets)
+                }
+            }
+            tr {
+                footer(users, bets)
+            }
+        }
+    }
 }
 
 fun main(args: Array<String>) {
@@ -35,86 +127,26 @@ fun main(args: Array<String>) {
         routing {
             get("/") {
                 call.respondHtml {
+                    val head = "WM Tipps Familie Kunz"
                     head {
-                        title("WM Tipps Kunz Familie")
-                    }
-                    unsafe {
-                        raw("""
-                            <style>
-table {
-    border-collapse: collapse;
-    width: 100%;
-}
-
-table, td, th {
-    height: 50px;
-    border: 1px solid black;
-}
-</style>
-                            """)
+                        title(head)
+                        styleLink("static/styles.css")
                     }
                     body {
-                        h1 { +"WM Tipps Kunz Familie" }
-                        p {
-                            table {
-
-                                val users = d.getUsers()
-                                    .filter { it != User.RESULT }
-                                val readAll = d.readAll()
-                                val (res, bets) = getBets(readAll)
-                                val results = res.sortedWith(compareBy { toLong(it.betKey.match.date) })
-                                tr {
-                                    td { +"Begegnung" }
-                                    td { +"Ergebnis" }
-                                    users.forEach {
-                                        td { +it.name }
-                                        td {}
-                                    }
-                                }
-                                tr {
-                                    td { }
-                                    td { }
-                                    users.forEach {
-                                        td { +"Tipp" }
-                                        td { +"Punkte" }
-                                    }
-                                }
-                                results.forEach { result ->
-                                    tr {
-                                        val match = result.betKey.match
-                                        td { +match.toString() }
-                                        td { +result.result.toString() }
-                                        users.forEach { user ->
-                                            bets.find { bet ->
-                                                bet.betData.betKey.user == user && match.equalsSameDayAndTeam(bet.betData.betKey.match)
-                                            }?.let {
-                                                td { +"${it.betData.result} " }
-                                                if (it.betData.betKey.user != User.RESULT) {
-                                                    td { +"${it.points} " }
-                                                }
-                                            } ?: run {
-                                                td { }
-                                                td { }
-                                            }
-                                        }
-                                    }
-                                }
-                                tr {
-                                    td { +"Zwischenstand: " }
-                                    users.forEach { user ->
-                                        val sum = bets.filter { it.betData.betKey.user == user }
-                                            .map { it.points }
-                                            .filter { it != Points.NONE }
-                                            .map(Points::int)
-                                            .sum()
-                                        td { +"${user.name}" }
-                                        td { +"$sum" }
-                                    }
-                                }
-                            }
-                        }
+                        thisBody(d, head)
                     }
                 }
+            }
+            static("static") {
+                //                staticRootFolder = File("/home/mheinrich/Software/workspace/Private/betevaluater/build/resources/main")
+//                staticRootFolder = File(".")
+//                staticRootFolder =File("css")
+                resources("css")
+                files("css")
+                default("index.html")
+            }
+            get("{...}") {
+                println("here it comes")
             }
         }
     }
